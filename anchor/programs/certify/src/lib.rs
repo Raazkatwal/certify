73d0d3caa850200a3c6a8795be1aea979e748386
Raazkatwal <raazkatwal5@gmail.com>
2026-02-16 23:34:51 +0545

    feat(Anchor): Add a minimal certificate issuer.

--- anchor/programs/certify/src/lib.rs
+++ anchor/programs/certify/src/lib.rs
@@ -6,11 +6,39 @@ declare_id!("HEYaFCGw2M4W9xnrXxzoByfQhHr5uRMHiYGM5MoQPKq4");
 pub mod certify {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn issue_certificate(
+        ctx: Context<IssueCertificate>,
+        recipient: Pubkey,
+        metadata_hash: String,
+    ) -> Result<()> {
+        let certificate = &mut ctx.accounts.certificate;
+
+        certificate.issuer = ctx.accounts.issuer.key();
+        certificate.recipient = recipient;
+        certificate.metadata_hash = metadata_hash;
+
         Ok(())
     }
 }
 
+#[account]
+pub struct Certificate {
+    pub issuer: Pubkey,
+    pub recipient: Pubkey,
+    pub metadata_hash: String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(recipient: Pubkey, metadata_hash: String)]
+pub struct IssueCertificate<'info> {
+    #[account(
+        init,
+        payer = issuer,
+        space = 8 + 32 + 32 + 4 + 128
+    )]
+    pub certificate: Account<'info, Certificate>,
+
+    #[account(mut)]
+    pub issuer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
